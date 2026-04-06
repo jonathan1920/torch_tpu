d192f3c7deb80014c0f775ceb16736160ae451d9
Chun-nien Chan <cnchan@google.com>
2026-04-06 10:14:55 -0700

    Enfore DeferAll mode in compile api unit tests to avoid materialization with DeferNever being the new default
    Generated from cl/895384447
    
    PiperOrigin-RevId: 895384447

--- tests/compile/compile_api_test.py
+++ tests/compile/compile_api_test.py
@@ -17,6 +17,7 @@
 from absl.testing import absltest
 import torch
 from torch_tpu import api
+from torch_tpu._internal import execution_mode
 from torch_tpu._internal.compile import tpu_torch_compile
 
 
@@ -32,13 +33,26 @@ def get_mock_lookup_backend():
   )
 
 
+def eager_mode_defer_all():
+  """Enable EagerMode.INTERNAL_DEFER_ALL.
+
+  This prevents tensor materialization during Python execution and enables
+  `build_mlir` to trace the deferred ops and generate MLIR.
+
+  Returns:
+    A context manager for setting the execution mode.
+  """
+  return execution_mode.eager_mode(execution_mode.EagerMode.INTERNAL_DEFER_ALL)
+
+
 # TODO: add more test coverage for the direct compile API.
 class CompileApiTest(absltest.TestCase):
 
   def test_build_mlir(self):
-    x = torch.ones(10, device='cpu').to(device=api.tpu_device())
-    y = torch.ones(10, device='cpu').to(device=api.tpu_device())
-    z = x + y
+    with eager_mode_defer_all():
+      x = torch.ones(10, device='cpu').to(device=api.tpu_device())
+      y = torch.ones(10, device='cpu').to(device=api.tpu_device())
+      z = x + y
     result_tensors = [z]
     argument_tensors = [x, y]
     donate_args = (1,)
@@ -54,10 +68,11 @@ class CompileApiTest(absltest.TestCase):
     self.assertIn('stablehlo.add', mlir_str)
 
   def test_extra_input_to_build_mlir(self):
-    x = torch.ones(10, device='cpu').to(device=api.tpu_device())
-    y = torch.ones(10, device='cpu').to(device=api.tpu_device())
-    extra = torch.ones(10, device='cpu').to(device=api.tpu_device())
-    z = x + y
+    with eager_mode_defer_all():
+      x = torch.ones(10, device='cpu').to(device=api.tpu_device())
+      y = torch.ones(10, device='cpu').to(device=api.tpu_device())
+      extra = torch.ones(10, device='cpu').to(device=api.tpu_device())
+      z = x + y
     result_tensors = [z]
     argument_tensors = [x, y, extra]
     mlir = tpu_torch_compile.build_mlir(result_tensors, argument_tensors)
@@ -66,9 +81,10 @@ class CompileApiTest(absltest.TestCase):
     self.assertIn('%arg2', mlir_str)
 
   def test_missing_input_to_build_mlir(self):
-    x = torch.ones(10, device='cpu').to(device=api.tpu_device())
-    y = torch.ones(10, device='cpu').to(device=api.tpu_device())
-    z = x + y
+    with eager_mode_defer_all():
+      x = torch.ones(10, device='cpu').to(device=api.tpu_device())
+      y = torch.ones(10, device='cpu').to(device=api.tpu_device())
+      z = x + y
     result_tensors = [z]
     argument_tensors = [x]
     with self.assertRaises(RuntimeError) as err:
@@ -102,6 +118,7 @@ class CompileApiTest(absltest.TestCase):
 
   def test_compile_default_backend_no_recompilations(self):
     num_calls = 0
+
     def user_context() -> str:
       nonlocal num_calls
       num_calls += 1
