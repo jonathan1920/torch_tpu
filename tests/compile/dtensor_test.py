4ee6463b98b18c3cee9a2601d964135deed02b8d
Arian Arfaian <aarfaian@google.com>
2026-04-08 16:06:49 -0700

    Add DTensor torch.compile bug minimal reproducer.
    Compiling `fn` will produce an FX graph with a `FakeScriptObject` arg in
    `forward`, which is a `DeviceMesh` instance when executed.
    
    Generated from cl/896752210
    
    PiperOrigin-RevId: 896752210

---
+++ tests/compile/dtensor_test.py
@@ -0,0 +1,58 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import absltest
+import torch
+from torch.testing._internal.distributed.fake_pg import FakeStore
+from torch_tpu import api
+
+
+class DTensorTest(absltest.TestCase):
+
+  def setUp(self):
+    self._world_size = 2
+    super().setUp()
+    torch.distributed.init_process_group(
+        backend="fake", store=FakeStore(), rank=0, world_size=self._world_size
+    )
+    _ = api.tpu_device()
+
+  def tearDown(self):
+    torch.distributed.destroy_process_group()
+    super().tearDown()
+
+  @absltest.skip(
+      "b/499316489 -- issue with handling DeviceMesh/FakeScriptObject"
+      " inputs/outputs during compile"
+  )
+  def test_dtensor_basic(self):
+    mesh = torch.distributed.tensor.DeviceMesh(
+        "tpu", torch.arange(self._world_size)
+    )
+
+    @torch.compile(fullgraph=True)
+    def fn(x):
+      return x * x + 2
+
+    param = torch.randn(4, 4, requires_grad=True)
+    x = torch.distributed.tensor.DTensor.from_local(
+        param, mesh, [torch.distributed.tensor.Shard(0)], run_check=False
+    )
+
+    res = fn(x)
+    res.to_local().sum().backward()
+
+
+if __name__ == "__main__":
+  absltest.main()
