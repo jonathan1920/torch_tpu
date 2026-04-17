b519dcfe8427542a5c0ec2afdf17f16bc49961e2
Paul Ganssle <pganssle@google.com>
2026-04-17 10:59:05 -0700

    Add Apache 2.0 header to mingpt_training_example.py
    We have made modifications to this file, and so the new license also applies. Since the old license also applies, it must remain in place as well.
    
    Generated from cl/901376795
    
    PiperOrigin-RevId: 901376795

--- examples/mingpt/mingpt_training_example.py
+++ examples/mingpt/mingpt_training_example.py
@@ -21,6 +21,24 @@ From the original minGPT implementation:
 https://github.com/karpathy/minGPT/blob/master/demo.ipynb
 """
 
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Note: This file has two licenses because it is a derivative work of an
+# MIT-licensed file. We have left the original license notice intact and added
+# an Apache license header to cover modifications.
+
 from collections.abc import Sequence
 import pickle
 
