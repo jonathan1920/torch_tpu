cd6cb595d1970ef75ee55a1b3b4f740ddddf678c
Manish Khanna <mkkhanna@google.com>
2026-04-21 13:50:23 -0700

    Add Qwen3 dynamic KV cache example
    This example is a copy of `qwen3_hf_example.py` with the difference that this example uses DynamicCache for KV caching and passes single new token to the model for every decode step instead of concatenating all the previous tokens.
    
    Generated from cl/903407368
    
    PiperOrigin-RevId: 903407368

---
+++ examples/qwen3/qwen3_dynamic_kv_cache.py
@@ -0,0 +1,139 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Example use of Qwen3 model with KV Cache."""
+
+import time
+
+from absl import app
+from absl import logging
+import torch
+from torch_tpu import api
+from torch_tpu._internal.utils import log_utils
+from examples import paths
+import transformers
+
+log_utils.log_to_stderr()
+
+
+#  for backend registration
+
+TOKENIZER_PATH = f"{paths.XM_HOME}weights/huggingface/Qwen/Qwen3-0.6B"
+MODEL_PATH = f"{paths.XM_HOME}weights/huggingface/Qwen/Qwen3-0.6B"
+
+
+def model_generate(
+    model, initial_inputs: torch.Tensor, tokenizer, max_decode_steps
+):
+  with torch.no_grad():
+    start_time = time.time()
+    output = model(input_ids=initial_inputs, use_cache=True)
+    end_time = time.time()
+    logits = output.logits
+    past_key_values = output.past_key_values
+
+    prefill_time = end_time - start_time
+    print(f"Prefill time: {prefill_time * 1000:.2f} ms")
+    output_tokens = initial_inputs
+    next_token = None
+    start_time = time.time()
+    steps_completed = 0
+    for i in range(max_decode_steps):
+      print(f"Decode step {i}, device={logits.device}")
+      if next_token == 0:
+        break
+      # greedy sampling
+      next_token = torch.argmax(logits[:, -1, :], dim=-1).unsqueeze(-1)
+      output_tokens = torch.cat([output_tokens, next_token], dim=1)
+
+      # Only pass the next token and the past_key_values
+      output = model(
+          input_ids=next_token, past_key_values=past_key_values, use_cache=True
+      )
+      logits = output.logits
+      past_key_values = output.past_key_values
+      steps_completed += 1
+
+    end_time = time.time()
+    decode_time = end_time - start_time
+    if steps_completed > 0:
+      print(
+          "Decode time per token:"
+          f" {decode_time * 1000 / steps_completed:.2f} ms"
+      )
+    else:
+      print("No decode steps were run.")
+  output = output_tokens
+  output_list = output.tolist()
+  output_text = tokenizer.decode(output_list[0], skip_special_tokens=True)
+  return output_text
+
+
+# pylint: disable=unused-argument
+def main(argv):
+  # Initialize TorchTPU
+  tpu_device = api.tpu_device()
+  torch.manual_seed(123)
+
+  # Load tokenizer from HuggingFace
+  tokenizer = transformers.AutoTokenizer.from_pretrained(TOKENIZER_PATH)
+
+  # Load CPU model from HuggingFace
+  model_cpu = transformers.AutoModelForCausalLM.from_pretrained(
+      MODEL_PATH, torch_dtype=torch.bfloat16, attn_implementation="eager"
+  )
+  assert str(model_cpu.device) == "cpu", "model_cpu device should be cpu"
+  assert model_cpu.dtype == torch.bfloat16, "model_cpu dtype should be bfloat16"
+
+  # Load CPU model from HuggingFace and move it to the TPU
+  model_tpu = transformers.AutoModelForCausalLM.from_pretrained(
+      MODEL_PATH, torch_dtype=torch.bfloat16, attn_implementation="eager"
+  )
+  model_tpu = model_tpu.to("tpu")
+  assert (
+      str(model_tpu.device) == f"{tpu_device}:0"
+  ), f"model_tpu device should be {tpu_device}:0, got {model_tpu.device}"
+  assert model_tpu.dtype == torch.bfloat16, "model_tpu dtype should be bfloat16"
+
+  text = "Who are you?"
+  messages = [{"role": "user", "content": text}]
+  inputs = tokenizer.apply_chat_template(
+      messages,
+      add_generation_prompt=True,
+      tokenize=True,
+      return_dict=True,
+      return_tensors="pt",
+  ).input_ids
+
+  max_decode_steps = 2
+
+  output_cpu = model_generate(model_cpu, inputs, tokenizer, max_decode_steps)
+  logging.info("output_cpu=%s", output_cpu)
+
+  output_tpu = model_generate(
+      model_tpu, inputs.to("tpu"), tokenizer, max_decode_steps
+  )
+  logging.info("output_tpu=%s", output_tpu)
+  assert output_cpu == output_tpu
+
+  model_tpu_compiled = torch.compile(model_tpu, dynamic=False, backend="tpu")
+  output_tpu_compiled = model_generate(
+      model_tpu_compiled, inputs.to("tpu"), tokenizer, max_decode_steps
+  )
+  logging.info("output_tpu_compiled=%s", output_tpu_compiled)
+  assert output_cpu == output_tpu_compiled
+
+
+if __name__ == "__main__":
+  app.run(main)
