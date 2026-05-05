e85cc56fd3d977ddaa12a1cc7c13860c68ef6f3f
Zhanyong Wan <wan@google.com>
2026-05-05 09:25:54 -0700

    Change `kTpuContextSlot` from `constexpr` to `const`.
    `TEST_INFO` is changed from `constexpr` to `const` in upstream. Therefore we must change our use to `const` as well to avoid breaking the build when we upgrade TorchTPU the next time.
    
    Generated from cl/910734703
    
    PiperOrigin-RevId: 910734703

--- torch_tpu/common/context_manager.h
+++ torch_tpu/common/context_manager.h
@@ -33,7 +33,7 @@ namespace internal {
 // can only use the TEST_INFO slot.
 // TODO(https://github.com/pytorch/pytorch/issues/56027): use a dedicated slot
 // for each type of context manager.
-inline constexpr auto kTpuContextSlot = c10::DebugInfoKind::TEST_INFO;
+inline const auto kTpuContextSlot = c10::DebugInfoKind::TEST_INFO;
 }  // namespace internal
 
 // The state for different types of context managers respectively.
