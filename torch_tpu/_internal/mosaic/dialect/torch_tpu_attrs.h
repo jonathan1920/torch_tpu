41acfbcf49d183d9fa9532847fd22d4a485c7457
Will Froom <willfroom@google.com>
2026-06-05 08:33:42 -0700

    Introduce placeholder dialect for Mosaic integration.
    We will use this as a thin layer over Mosaic. This will act as a single point of contact, easing migration for Mosaic development. It also allows us to add our own abstractions / transformations.
    
    Generated from cl/927315895
    
    PiperOrigin-RevId: 927315895

---
+++ torch_tpu/_internal/mosaic/dialect/torch_tpu_attrs.h
@@ -0,0 +1,23 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TORCH_TPU_INTERNAL_MOSAIC_DIALECT_TORCH_TPU_ATTRS_H_
+#define TORCH_TPU_INTERNAL_MOSAIC_DIALECT_TORCH_TPU_ATTRS_H_
+
+#include "mlir/IR/Attributes.h"
+
+#define GET_ATTRDEF_CLASSES  // NON_TT_MACRO_OK=MLIR gen
+#include "torch_tpu/_internal/mosaic/dialect/torch_tpu_attrs.h.inc"
+
+#endif  // TORCH_TPU_INTERNAL_MOSAIC_DIALECT_TORCH_TPU_ATTRS_H_
---
+++ torch_tpu/_internal/mosaic/dialect/torch_tpu_dialect.h
@@ -0,0 +1,21 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TORCH_TPU_INTERNAL_MOSAIC_DIALECT_TORCH_TPU_DIALECT_H_
+#define TORCH_TPU_INTERNAL_MOSAIC_DIALECT_TORCH_TPU_DIALECT_H_
+
+#include "mlir/IR/Dialect.h"
+#include "torch_tpu/_internal/mosaic/dialect/torch_tpu_dialect.h.inc"
+
+#endif  // TORCH_TPU_INTERNAL_MOSAIC_DIALECT_TORCH_TPU_DIALECT_H_
