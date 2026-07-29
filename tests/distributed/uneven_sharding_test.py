c1187dda0e0c0b72ab0f29edbc3a4d04fe71ea56
Tushar Agarwal <agarwaltushar@google.com>
2026-07-29 12:48:57 -0700

    Add multi-dimensional tensor tests to demonstrate TPU hang when redistributing unevenly sharded tensors.
    Generated from cl/956037049
    
    PiperOrigin-RevId: 956037049

--- tests/distributed/uneven_sharding_test.py
+++ tests/distributed/uneven_sharding_test.py
@@ -29,7 +29,11 @@ from torch_tpu._internal.shims.pyglib.contrib.g3_multiprocessing import g3_multi
 
 
 def _replicate_tensor(
-    tensor_size: int, compiled: bool, pass_shape: bool = False
+    tensor_size: int,
+    compiled: bool,
+    ndims: int = 1,
+    pass_shape: bool = False,
+    trailing_dim_size: int = 4,
 ) -> None:
   """Redistributes a tensor from shard to replicate."""
   dist.init_process_group(backend="tpu_dist")
@@ -37,72 +41,79 @@ def _replicate_tensor(
   world_size = int(os.environ["WORLD_SIZE"])
   device_mesh = dt.init_device_mesh("tpu", (world_size,))
 
+  trailing_dims = [trailing_dim_size] * (ndims - 1)
+
   def shard_and_redistribute(x):
     if pass_shape:
-      global_shape = torch.Size((tensor_size,))
+      global_shape = torch.Size([tensor_size] + trailing_dims)
       global_stride = torch.empty(global_shape, device="meta").stride()
       dtensor = dt.DTensor.from_local(
           x,
-          device_mesh,
-          [dt.Shard(0)],
+          device_mesh=device_mesh,
+          placements=[dt.Shard(0)],
           shape=global_shape,
           stride=global_stride,
       )
     else:
       dtensor = dt.DTensor.from_local(x, device_mesh, [dt.Shard(0)])
+    return dtensor.redistribute(device_mesh, [dt.Replicate()]).to_local()
 
-    replicated = dtensor.redistribute(device_mesh, [dt.Replicate()])
-    return replicated.to_local()
-
-  # Create local tensor. We follow the torch.chunk strategy so that DTensor can
-  # be correctly reconstructed. torch.chunk splits into max-size chunks, leaving
-  # the last few ranks -- if any -- empty (size 0).
   chunk_size = (tensor_size + world_size - 1) // world_size
   start = min(rank * chunk_size, tensor_size)
   end = min((rank + 1) * chunk_size, tensor_size)
   local_size = end - start
-  local_tensor = torch.ones(local_size, dtype=torch.float32, device="tpu")
+  local_shape = [local_size] + trailing_dims
+  local_tensor = torch.ones(*local_shape, dtype=torch.float32, device="tpu")
 
   if compiled:
     backend = tt_compile.TpuBackend(debug=True)
-    func = torch.compile(
-        shard_and_redistribute, backend=backend, fullgraph=True
-    )
+    func = torch.compile(shard_and_redistribute, backend=backend)
   else:
     func = shard_and_redistribute
 
   output = func(local_tensor)
   out = output.cpu()
 
-  expected = torch.ones(tensor_size, dtype=torch.float32)
-  utils.assert_close(out, expected)
+  if pass_shape or rank == 0:
+    expected_shape = [tensor_size] + trailing_dims
+    expected = torch.ones(*expected_shape, dtype=torch.float32)
+    utils.assert_close(out, expected)
 
 
 class UnevenShardingTest(parameterized.TestCase):
 
   @parameterized.named_parameters(
-      ("eager_small", False, 4),
-      ("compiled_small", True, 4),
-      ("eager_uneven", False, 12),
-      ("compiled_uneven", True, 12),
+      ("1d_eager_small", 1, False, 4),
+      ("1d_compiled_small", 1, True, 4),
+      ("1d_eager_uneven", 1, False, 12),
+      ("1d_compiled_uneven", 1, True, 12),
+      ("2d_eager_small", 2, False, 4),
+      ("2d_compiled_small", 2, True, 4),
+      ("2d_eager_uneven", 2, False, 12),
+      ("2d_compiled_uneven", 2, True, 12),
   )
-  def test_replicate_tensor_with_shape(self, compiled, tensor_size):
+  def test_replicate_tensor_with_shape(self, ndims, compiled, tensor_size):
     distributed_utils.dist_run(
         nproc_per_node=8,
         fn=singlehost_wrapper.tpu_env_wrapper(_replicate_tensor, world_size=8),
         tensor_size=tensor_size,
         compiled=compiled,
+        ndims=ndims,
         pass_shape=True,
     )
 
   @parameterized.named_parameters(
-      ("compiled_fail_small", True, 4, "Tensor shape mismatch"),
-      ("eager_fail_small", False, 4, "Tensor shape mismatch"),
-      ("compiled_fail_uneven", True, 12, "Tensor shape mismatch"),
-      ("eager_fail_uneven", False, 12, "Tensor shape mismatch"),
+      ("1d_compiled_fail_small", 1, True, 4, "Tensor shape mismatch"),
+      ("1d_eager_fail_small", 1, False, 4, "Tensor shape mismatch"),
+      ("1d_compiled_fail_uneven", 1, True, 12, "Tensor shape mismatch"),
+      ("1d_eager_fail_uneven", 1, False, 12, "Tensor shape mismatch"),
+      ("2d_compiled_fail_small", 2, True, 4, "Wait timeout on sflag"),
+      ("2d_eager_fail_small", 2, False, 4, "Wait timeout on sflag"),
+      ("2d_compiled_fail_uneven", 2, True, 12, "Wait timeout on sflag"),
+      ("2d_eager_fail_uneven", 2, False, 12, "Wait timeout on sflag"),
   )
   def test_replicate_tensor_without_shape(
-      self, compiled, tensor_size, expected_regex
+      self, ndims, compiled, tensor_size, expected_regex
   ):
     with self.assertRaisesRegex(Exception, expected_regex):
       distributed_utils.dist_run(
@@ -112,6 +123,7 @@ class UnevenShardingTest(parameterized.TestCase):
           ),
           tensor_size=tensor_size,
           compiled=compiled,
+          ndims=ndims,
           pass_shape=False,
       )
 
