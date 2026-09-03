2fd331f065574dbbd4d9d731cf71e9168f6d3880
Lee Howes <lwh@google.com>
2026-09-02 18:22:27 -0700

    singlehost_wrapper_test was disabled in github. Reenable it.
    Generated from cl/975436527
    
    PiperOrigin-RevId: 975436527

--- torch_tpu/_internal/distributed/launchers/singlehost_wrapper_test.py
+++ torch_tpu/_internal/distributed/launchers/singlehost_wrapper_test.py
@@ -17,27 +17,29 @@
 import os
 
 from absl.testing import absltest
-from torch.google import distributed as g3_distributed
 import torch.multiprocessing as mp
 from torch_tpu._internal.distributed.launchers import singlehost_wrapper
 from torch_tpu._internal.distributed import multiprocessing
 from tests import seed_test_utils
+import tests.distributed.distributed_utils as distributed_utils
 
 
 WORLD_SIZE = 8
 
 
-def worker_func(rank, world_size):
+def worker_func(world_size):
   mandatory_vars = [
       "WORLD_SIZE",
-      "RANK",
       "LOCAL_RANK",
       "MASTER_ADDR",
       "MASTER_PORT",
       "TORCH_TPU_SLICEBUILDER_ADDRESSES",
-      "TORCH_TPU_SLICEBUILDER_PORT",
       "TORCH_TPU_TOPOLOGY",
   ]
+
+  if "RANK" not in os.environ:
+    raise RuntimeError("Mandatory variable RANK not set in worker")
+  rank = int(os.environ["RANK"])
   for var in mandatory_vars:
     if var not in os.environ:
       raise RuntimeError(f"Mandatory variable {var} not set in worker {rank}")
@@ -48,9 +50,9 @@ def worker_func(rank, world_size):
         f"WORLD_SIZE mismatch: expected {world_size}, got"
         f" {os.environ['WORLD_SIZE']}"
     )
-  if int(os.environ["RANK"]) != rank:
+  if rank >= world_size:
     raise ValueError(
-        f"RANK mismatch: expected {rank}, got {os.environ['RANK']}"
+        f"RANK out of bounds: expected rank less than {world_size}, got {rank}"
     )
   if int(os.environ["LOCAL_RANK"]) != rank:
     raise ValueError(
@@ -64,7 +66,6 @@ def worker_func(rank, world_size):
 
   # Ensure ports are set and represent integers
   int(os.environ["MASTER_PORT"])
-  int(os.environ["TORCH_TPU_SLICEBUILDER_PORT"])
 
   # Ensure topology and addresses are non-empty
   if not os.environ["TORCH_TPU_TOPOLOGY"]:
@@ -89,9 +90,12 @@ class SingleHostTestLauncherTest(seed_test_utils.RepeatableTest):
           self.create_tempdir().full_path
       )
 
-    g3_distributed.torchrun(
-        singlehost_wrapper.tpu_env_wrapper(worker_func, world_size=WORLD_SIZE),
-        nproc_per_node=8,
+    distributed_utils.dist_run(
+        nproc_per_node=WORLD_SIZE,
+        fn=singlehost_wrapper.tpu_env_wrapper(
+            worker_func, world_size=WORLD_SIZE
+        ),
+        world_size=WORLD_SIZE,
     )
 
   def test_tpu_env_wrapper_no_args(self):
