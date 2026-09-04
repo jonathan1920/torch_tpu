2f4afbe39288834807ba393e6073eed346769d79
Yiling Yuan <yilingyuan@google.com>
2026-09-04 12:43:11 -0700

    [tt_api] Migrate existing use sites of TPU_DEFER_AND_FUSE to TORCH_TPU_DEFER_AND_FUSE
    Generated from cl/976447649
    
    PiperOrigin-RevId: 976447649

--- tests/eager/xla_compile_options_test.py
+++ tests/eager/xla_compile_options_test.py
@@ -35,7 +35,7 @@ class XlaCompileOptionsTest(
     # Pop any env vars that affect global eager mode default.
     # See go/tt-knobs#eager-mode.
     os.environ.pop("TPU_LAUNCH_BLOCKING", None)
-    os.environ.pop("TPU_DEFER_AND_FUSE", None)
+    os.environ.pop("TORCH_TPU_DEFER_AND_FUSE", None)
 
     if "TEST_UNDECLARED_OUTPUTS_DIR" not in os.environ:
       os.environ["TEST_UNDECLARED_OUTPUTS_DIR"] = (
